8910354c0cc892d81fdbeab4f64c874108854bdd
Mateo Awon-Magliaro <m.awonmagliaro@gmail.com>
2025-03-24 16:38:43 -0400

    part 1: added variables and functions to main.js

--- Assignment 4/part 1/main.js
+++ Assignment 4/part 1/main.js
@@ -1,3 +1,13 @@
 // Mateo Awon-Magliaro
-// 2025-03-24
+// Date created: 2025-03-24
 // Description: Random story generator
+
+
+// variables and functions
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
