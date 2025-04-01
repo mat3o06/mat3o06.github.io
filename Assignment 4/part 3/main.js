05ccd09e2fa87772a72873c17467a227f8ecd2db
Mateo Awon-Magliaro <m.awonmagliaro@gmail.com>
2025-04-01 16:07:06 -0400

    part 3: added the first part of the script

--- Assignment 4/part 3/main.js
+++ Assignment 4/part 3/main.js
@@ -2,3 +2,8 @@
 //2025-04-01
 //Description: Webpage that displays bouncing balls on the screen
 
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
